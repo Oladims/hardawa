0b369b2fc20d3bce711f1000ae50fab393ed881f
Ibrahim Olufowora <ibrahim.olufowora@Interswitchng.com>
2020-11-15 10:21:38 +0100

    Update views

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,53 +1,105 @@
 /* globals Chart:false, feather:false */
 
 (function () {
-  'use strict'
+  ("use strict");
 
-  feather.replace()
+  feather.replace();
 
   // Graphs
-  var ctx = document.getElementById('myChart')
+  var ctx = document.getElementById("myChart");
+  // Graphs
+  var ctx2 = document.getElementById("myChart2");
   // eslint-disable-next-line no-unused-vars
   var myChart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+      labels: ["18-22", "23-27", "28-35", "35-40", "40-50"],
+      datasets: [
+        {
+          data: [5339, 1345, 8483, 4003, 3489, 4092, 2034],
+          lineTension: 0,
+          backgroundColor: "transparent",
+          borderColor: "#007bff",
+          borderWidth: 4,
+          pointBackgroundColor: "#007bff",
+        },
       ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
     },
     options: {
       scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: false,
+            },
+          },
+        ],
       },
       legend: {
-        display: false
-      }
-    }
-  })
-})()
+        display: false,
+      },
+    },
+  });
+
+  // And for a doughnut chart
+  var myChart2 = new Chart(ctx2, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [50, 30, 10],
+          backgroundColor: [
+            'red',
+            'orange',
+            'yellow',
+            
+          ],
+        },
+      ],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: ["Male", "Female", "Bi"],
+    },
+    // options: options,
+  });
+
+  // var config = {
+  //   type: "doughnut",
+  //   data: {
+  //     datasets: [
+  //       {
+  //         data: [
+  //           randomScalingFactor(),
+  //           randomScalingFactor(),
+  //           randomScalingFactor(),
+  //           randomScalingFactor(),
+  //           randomScalingFactor(),
+  //         ],
+  //         backgroundColor: [
+  //           window.chartColors.red,
+  //           window.chartColors.orange,
+  //           window.chartColors.yellow,
+  //           window.chartColors.green,
+  //           window.chartColors.blue,
+  //         ],
+  //         label: "Dataset 1",
+  //       },
+  //     ],
+  //     labels: ["Red", "Orange", "Yellow", "Green", "Blue"],
+  //   },
+  //   options: {
+  //     responsive: true,
+  //     legend: {
+  //       position: "top",
+  //     },
+  //     title: {
+  //       display: true,
+  //       text: "Chart.js Doughnut Chart",
+  //     },
+  //     animation: {
+  //       animateScale: true,
+  //       animateRotate: true,
+  //     },
+  //   },
+  // };
+})();
